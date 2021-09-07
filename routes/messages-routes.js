cf59e46cbb3779bdab20afc36cd7f30354e33175
mart8ins <mart8ins@gmail.com>
2021-09-07 17:21:16 +0300

    cleaned code in messages-routes/controller

--- routes/messages-routes.js
+++ routes/messages-routes.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { sendMessage,
-        getAllConversations,
-        getMessageFeed,
-        setAllMessagesAsRead} = require("../controlers/messages-controler");
+const { sendMessage } = require("../controlers/messages-controler");
 const {AuthCheck} = require("../middleware/auth-check");
 
 
-router.get("/messages/feed", AuthCheck, getMessageFeed); // get message feed for conversation
-router.get("/messages", AuthCheck, getAllConversations); // get all conversations for user
-
 router.post("/messages/new", AuthCheck, sendMessage); // send message to user
-router.post("/messages", AuthCheck, setAllMessagesAsRead); // set all message as read
 
 
 
