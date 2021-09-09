77b888900caebba1a476754fcbcbf3ff038b3e4e
mart8ins <mart8ins@gmail.com>
2021-09-09 16:17:45 +0300

    added and finished changeProfilePassword function

--- controlers/profile-controler.js
+++ controlers/profile-controler.js
@@ -1,12 +1,14 @@
 const User = require("../models/User");
 const HttpError = require("../errors/HttpError");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const changeProfileData = async (req, res, next) => {
-    const {userId} = req.userData;
-    const { firstName, lastName, email } = req.body.data;
-    const emailExists = await User.findOne({email: email});
-
     try {
+        const {userId} = req.userData;
+        const { firstName, lastName, email } = req.body.data;
+        const emailExists = await User.findOne({email: email});
+
         if(emailExists && String(emailExists._id) !== String(userId)) {
             res.json({error: "Email already in use."})
         } else {
@@ -23,4 +25,26 @@ const changeProfileData = async (req, res, next) => {
     }
 }
 
+const changeProfilePassword = async (req, res, next) => {
+    try{
+    const {userId} = req.userData;
+    const {oldPassword, newPassword, repeatNewPassword} = req.body.data;
+    const user = await User.findOne({_id: userId});
+
+    const oldPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
+    if(oldPasswordCorrect && newPassword === repeatNewPassword) {
+        const hash = await bcrypt.hash(newPassword, saltRounds);
+        user.password = hash;
+        await user.save();
+        res.json({message: "Password changed!"});
+    } else {
+        res.json({error: "Failed to change password. Check you data."});
+    }
+    } catch(e){
+        const error = new HttpError("Failed to change password.", 400);
+        next(error);
+    }
+}
+
 module.exports.changeProfileData = changeProfileData;
+module.exports.changeProfilePassword = changeProfilePassword;
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {AuthCheck} = require("../middleware/auth-check");
-const {changeProfileData} = require("../controlers/profile-controler");
+const {changeProfileData, changeProfilePassword} = require("../controlers/profile-controler");
 
-router.post("/profile/change/userdata", AuthCheck, changeProfileData)
+router.post("/profile/change/userdata", AuthCheck, changeProfileData);
+router.post("/profile/change/password", AuthCheck, changeProfilePassword);
 
 module.exports = router;
